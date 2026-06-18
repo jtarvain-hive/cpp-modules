5b59f548688d0811e71206157c9050882325e13d
Joonatan Tarvainen <jtarvain@student.hive.fi>
2026-06-18 17:26:03 +0300

    cpp01 ex02 completed

---
+++ cpp01/ex02/src/main.cpp
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: jtarvain <jtarvain@student.hive.fi>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2026/06/18 16:33:02 by jtarvain          #+#    #+#             */
+/*   Updated: 2026/06/18 17:25:28 by jtarvain         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int	main(void)
+{
+	std::string	string = "HI THIS IS BRAIN";
+	std::string	*stringPTR = &string;
+	std::string	&stringREF = string;
+
+	std::cout << "memory address of string variable: " << &string << std::endl;
+	std::cout << "memory address held by stringPTR: " << stringPTR << std::endl;
+	std::cout << "memory address held by stringREF: " << &stringREF << std::endl;
+
+	std::cout << std::endl;
+
+	std::cout << "value of the string variable: " << string << std::endl;
+	std::cout << "value pointed to by stringPTR: " << *stringPTR << std::endl;
+	std::cout << "value pointed to by stringREF: " << stringREF << std::endl;
+	return (0);
+}
