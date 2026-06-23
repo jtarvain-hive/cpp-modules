9cb269df2409a88a46d9b239363ef40c5d0be464
Joonatan Tarvainen <jtarvain.hive@gmail.com>
2026-06-23 21:11:07 +0300

    cpp01 ex05 ready

--- cpp01/ex05/src/main.cpp
+++ cpp01/ex05/src/main.cpp
@@ -6,11 +6,25 @@
 /*   By: jtarvain <jtarvain@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/06/22 19:38:07 by jtarvain          #+#    #+#             */
-/*   Updated: 2026/06/22 19:38:24 by jtarvain         ###   ########.fr       */
+/*   Updated: 2026/06/23 20:49:18 by jtarvain         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "Harl.hpp"
+#include <iostream>
+
 int	main(void)
 {
+	Harl	harl;
+
+	harl.complain("DEBUG");
+	std::cout << std::endl;
+	harl.complain("INFO");
+	std::cout << std::endl;
+	harl.complain("WARNING");
+	std::cout << std::endl;
+	harl.complain("ERROR");
+	std::cout << std::endl;
+	
 	return (0);
 }
